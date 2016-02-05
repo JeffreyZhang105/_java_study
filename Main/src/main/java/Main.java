a68218d34954f21443b57a798a9f36a93f648d5f
張 景富 <zhangjf@hyron-js.com>
2016-02-05 17:51:23 +0800

    no message

---
+++ Main/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import psn.jeffreyzhang.thoughts.StringSimilarity;
+
+/**
+ * Created by zhangjf on 2016/02/05.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        new StringSimilarity().getSimilarityRatio("")
+    }
+}
