4418098d62e9347e53362add1878c23ad04e5303
aobajyoumylove <psn.jeffreyzhang@hotmail.com>
2016-03-17 20:37:54 +0800

    a few changes.

--- Main/src/main/java/Main.java
+++ Main/src/main/java/Main.java
@@ -1,9 +1,11 @@
+import psn.jeffreyzhang.thoughts.StringSimilarity;
+
 /**
  * Created by zhangjf on 2016/02/05.
  */
 public class Main {
 
     public static void main(String[] args){
-        //new StringSimilarity().getSimilarityRatio("")
+        float a = new StringSimilarity().getSimilarityRatio("aaaabbbb","ccccaaaa");
     }
 }
