8bbdb0cadc516b860f23a8b44ab223b7dd51724a
Jeffrey Zhang <psn.jeffreyzhang@hotmail.com>
2017-03-17 17:15:31 +0800

    Reconstruct using gradle.

---
+++ demos/src/main/java/psn/jeffreyzhang/jaza/demos/WebCrawler/MyData.java
@@ -0,0 +1,21 @@
+package psn.jeffreyzhang.jaza.demos.WebCrawler;
+
+/**
+ * 数据
+ */
+public class MyData {
+    /**
+     * 数据名称
+     */
+    public final String name;
+
+    /**
+     * 数据值
+     */
+    public final String value;
+
+    public MyData(String newName, String newValue) {
+        name = newName;
+        value = newValue;
+    }
+}
